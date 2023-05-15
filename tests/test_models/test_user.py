577ce0e49b040231cc10fa8b1c52be621d4a4283
Peter Mgembe <peterrobsson@gmail.com>
2023-05-15 08:29:20 -0700

    added some tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,3 +8,15 @@ class TestUser(unittest.TestCase):
     def test_uid(self):
         self.assertEquals(type(User.id), type("hello"))
 
+    def test_email(self):
+        self.assertEquals("@" in User.email, True)
+
+    def test_password(self):
+        self.assertEquals(type(User.password), type("hello"))
+
+    def test_first_name(self):
+        self.assertEquals(type(User.first_name), type("hello"))
+
+    def test_last_name(self):
+        self.assertEquals(type(User.last_name), type("hello"))
+
