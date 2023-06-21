a74dd6cc3c7a488ea69fa36ee401233a117e1694
Peter Mgembe <peterrobsson@gmail.com>
2023-06-21 17:56:55 +0300

    solved and corrected question 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- Creating a database hbnb_dev_db
 -- and a new user hbnb_dev
 CREATE DATABASE `hbnb_dev_db` IF NOT EXISTS;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
